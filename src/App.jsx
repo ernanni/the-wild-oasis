22f0d39fda336815f08ebc08d4e5fcf4026e5dc1
Ernanni <ernannicf@gmail.com>
2024-01-08 03:24:09 -0300

    building the app layout

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import {
   Settings,
   Users,
 } from './pages';
+import { AppLayout } from './ui';
 
 function App() {
   return (
@@ -18,14 +19,16 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="account" element={<Account />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="dashboard" element={<Dashboard />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="account" element={<Account />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<Users />} />
+          </Route>
           <Route path="login" element={<Login />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="users" element={<Users />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
--- src/ui/index.js
+++ src/ui/index.js
@@ -1,6 +1,9 @@
+import AppLayout from './AppLayout';
 import Button from './Button';
+import Header from './Header';
 import Heading from './Heading';
 import Input from './Input';
 import Row from './Row';
+import Sidebar from './Sidebar';
 
-export { Button, Heading, Input, Row };
+export { AppLayout, Button, Header, Heading, Input, Row, Sidebar };
