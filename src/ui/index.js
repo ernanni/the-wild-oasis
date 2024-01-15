04a429c00047c7ffec67a90f6f3d55b177ad5c91
Ernanni <ernannicf@gmail.com>
2024-01-15 02:09:58 -0300

    refactor form row component

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,10 +1,27 @@
 import AppLayout from './AppLayout';
 import Button from './Button';
+import FileInput from './FileInput';
+import Form from './Form';
+import FormRow from './FormRow';
 import Header from './Header';
 import Heading from './Heading';
 import Input from './Input';
 import Row from './Row';
 import Sidebar from './Sidebar';
 import Spinner from './Spinner';
+import Textarea from './Textarea';
 
-export { AppLayout, Button, Header, Heading, Input, Row, Sidebar, Spinner };
+export {
+  AppLayout,
+  Button,
+  Form,
+  FileInput,
+  FormRow,
+  Header,
+  Heading,
+  Input,
+  Row,
+  Sidebar,
+  Spinner,
+  Textarea,
+};
