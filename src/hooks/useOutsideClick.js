fc602ab682efe796df8adb9f233482bb57481427
Ernanni <ernannicf@gmail.com>
2024-01-24 03:14:43 -0300

    added a click outside modal listenner and a hook for it

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from 'react';
+
+export function useOutsideClick(handler, listenCapturing = true) {
+  const ref = useRef();
+
+  useEffect(() => {
+    function handleClick(e) {
+      if (ref.current && !ref.current.contains(e.target)) handler();
+    }
+    // the true flag is to handle the click only in the capturing phase (as the event moves down the tree)
+    document.addEventListener('click', handleClick, listenCapturing);
+
+    return () =>
+      document.removeEventListener('click', handleClick, listenCapturing);
+  }, [handler]);
+
+  return ref;
+}
