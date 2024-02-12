79e39c60fde942057a5332aad0bcb601667182d0
Ernanni <ernannicf@gmail.com>
2024-02-12 17:22:19 -0300

    running pre fetching

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
 import { getBookings } from "../../services/apiBookings";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   // FILTER
@@ -28,6 +30,20 @@ export function useBookings() {
     queryKey: ["bookings", filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
+  // PRE-FETCHING
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+  // Preventing unnecessary fetching if page doesn't exists
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
 
   return { isLoading, bookings, error, count };
 }
